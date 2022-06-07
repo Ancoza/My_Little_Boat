5d7f32d4ded3b0d49dd6b970aa049bf1c1b26283
Ancoza <mamm200213@gmail.com>
2022-06-07 16:41:24 -0600

    Menu Manager

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager SharedInstance;
+    [SerializeField]
+    private Canvas gameCanvas, storeCanvas, settingCanvas, mainCanvas;
+
+    void Awake()
+    {
+        if (SharedInstance == null)
+        {
+            SharedInstance = this;
+        }
+    }
+
+    void Start()
+    {
+        ShowMainMenu();
+        HideGameCanvas();
+        HideStoreCanvas();
+        HideSettingsMenu();
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    public void OpenSettings()
+    {
+        HideMainMenu();
+        ShowSettingMenu();
+    }
+
+    public void OpenStore()
+    {
+        HideMainMenu();
+        ShowStoreCanvas();
+    }
+
+    public void Play()
+    {
+        HideMainMenu();
+        ShowGameCanvas();
+    }
+
+    public void BackMain()
+    {
+        ShowMainMenu();
+        HideGameCanvas();
+        HideStoreCanvas();
+        HideSettingsMenu();
+    }
+    
+    //Game Canvas
+    public void ShowGameCanvas()
+    {
+        gameCanvas.enabled = true;
+    }
+    public void HideGameCanvas()
+    {
+        gameCanvas.enabled = false;
+    }
+    
+    //StoreCanvas
+    public void ShowStoreCanvas()
+    {
+        storeCanvas.enabled = true;
+    }
+    public void HideStoreCanvas()
+    {
+        storeCanvas.enabled = false;
+    }
+    
+    //SettingCanvas
+    public void ShowSettingMenu()
+    {
+        settingCanvas.enabled = true;
+    }
+    public void HideSettingsMenu()
+    {
+        settingCanvas.enabled = false;
+    }
+    
+    //MainCanvas
+    public void ShowMainMenu()
+    {
+        mainCanvas.enabled = true;
+    }
+    public void HideMainMenu()
+    {
+        mainCanvas.enabled = false;
+    }
+}
